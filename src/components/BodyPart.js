ff0d39de1157a7d875e5ead59073b45d1d5330ad
rahulrj147 <152685601+rahulrj147@users.noreply.github.com>
2025-01-09 02:25:27 +0530

    Update BodyPart.js

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -4,7 +4,7 @@ import Icon from '../assets/icons/42807.jpg';
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   <button
     type="button"
-    className={`flex flex-col items-center justify-center bg-gray-800 rounded-bl-[80px] rounded-tr-[80px] w-[270px] h-[282px] cursor-pointer gap-[47px] transition-transform transform hover:scale-110 hover:bg-gray-700 ${bodyPart === item ? 'border-t-4 border-blue-600' : ''}`}
+    className={`flex flex-col items-center justify-center bg-gray-800 rounded-bl-[80px] rounded-tr-[80px] w-[270px] h-[282px] cursor-none gap-[47px] transition-transform transform hover:scale-110 hover:bg-gray-700 ${bodyPart === item ? 'border-t-4 border-blue-600' : ''}`}
     onClick={() => {
       setBodyPart(item);
       window.scrollTo({ top: 1500, left: 100, behavior: 'smooth' });
